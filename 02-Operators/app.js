ba9b4bd2bf1890a5bfc2abc0d5f42feb73e50161
Manisha varma <manishavkapex1@gmail.com>
2024-07-21 14:08:24 +0530

    question added

---
+++ 02-Operators/app.js
@@ -0,0 +1,82 @@
+/*
+Task 1 : Write a program to add two numbers and log the result to the console
+ */
+
+
+/*
+Task 2 : Write a program to substract two numbers and log the result to the console
+ */
+
+/*
+Task 3 : Write a program to multiply two numbers and log the result to the console
+ */
+
+
+/*
+Task 4 : Write a program to divided two numbers and log the result to the console
+ */
+
+/*
+Task 5 : Write a program to find the remainder when one number is divided by another and log the result to the console.
+ */
+
+
+/*
+Task 6 : use the += operator to add a number to a variable and log the result to the console.
+ */
+
+
+/*
+Task 7 : Write a program to find the remainder when one number is divided by another and log the result.
+ */
+
+
+
+/*
+Task 8 : Write a program to compare two numbers using > and < and log the result.
+ */
+
+
+/*
+Task 9 : Write a program to compare two numbers using >= and =< and log the result.
+ */
+
+
+/*
+Task 10 : Write a program to compare two numbers using == and === and log the result.
+ */
+
+
+/*
+Task 11 : Write a program that uses the && operator to combine two conditions and log the result.
+ */
+
+
+/*
+Task 12 : Write a program that uses the || operator to combine two conditions and log the result.
+ */
+
+
+/*
+Task 13 : Write a program that uses the ! operator to negate a condition and log the result.
+ */
+
+
+/*
+Task 14 : Write a program that uses the ternary operator to check if a number is positive or negative and log the result.
+ */
+
+
+/*
+Task 15 : Write a script that perform basic arithmetic operations (addition, substraction, multiplication, division and remainder) on two numbers and log the results.
+ */
+
+
+/*
+Task 16 : Create a script that compares two numbers using different comparison operators and combines conditions using logical operators and log the result.
+ */
+
+
+/*
+Task 17 : Write a script that uses the ternary operator to determine if a number is positive or negative and log the result.
+ */
