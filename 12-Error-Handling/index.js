9545c9cca59b41507981f5757f8832dcbbaa734e
Manisha varma <manishavkapex1@gmail.com>
2024-07-29 21:19:01 +0530

    Questions Added

---
+++ 12-Error-Handling/index.js
@@ -0,0 +1,48 @@
+/*
+Task 1: Write a function that intentionally throws an error and use a try-catch block to handle the error and log an appropriate message to the console.
+*/
+
+
+
+/*
+Task 2: Create a function that divides two numbers and throw an error if the denominator is zero. Use a try-catch block to handle this error.
+*/
+
+
+
+/*
+Task 3: Write a script that included a try-catch block and a finally block. Log message in the try, catch and finally blocks to obeserve the execution flow.
+*/
+
+
+/*
+Task 4: Create a custom error class that extends the built-in Error class. Throw an instance of the custom error in a function and handle it using a try-catch block.
+*/
+
+
+/*
+Task 5: Write a function that validated user input (e.g, checking if a string is not empty) and throws a custom error if the validation fails. Handle the custom error using a try-catch block.
+*/
+
+
+/*
+Task 6: Create a promise that randomly resolves or rejects. Use .catch() to handle the rejection and log an appropriate error message to the console.
+*/
+
+
+
+/*
+Task 7: Use try-cathc within an async function to handle errors from a promise that randomly resolve or rejects, and log the error message.
+*/
+
+
+
+
+/*
+Task 8: Use the fetch API to request data from an invalid URL and handle the error using .catch(). Log an appropriate error message to the console. 
+*/
+
+
+/*
+Task 9: Use the fetch API to request data from an invalid URL within an async function and handle the error using try-catch. Log an appropriate error message.
+*/
