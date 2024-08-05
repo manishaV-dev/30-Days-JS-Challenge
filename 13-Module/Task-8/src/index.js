233030fe3505ac6a0c1d3ae0614afa8221bbdd40
Manisha varma <manishavkapex1@gmail.com>
2024-08-05 15:29:46 +0530

    day 13 completed

---
+++ 13-Module/Task-8/src/index.js
@@ -0,0 +1,7 @@
+// index.js
+import { add, subtract } from "./math.js";
+import { greet } from "./utils.js";
+
+console.log(add(10, 5)); // Output: 15
+console.log(subtract(10, 5)); // Output: 5
+console.log(greet("World")); // Output: Hello, World!
