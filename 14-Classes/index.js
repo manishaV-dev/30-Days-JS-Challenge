7eb249df4b25315d9fb37f540e284b62a5d34964
Manisha varma <manishavkapex1@gmail.com>
2024-08-06 11:05:49 +0530

    Day-14 questions added

---
+++ 14-Classes/index.js
@@ -0,0 +1,41 @@
+/*
+Task 1: Define a class Person with properties name and age, and a method to return a greeting message. Create an instance of a class and log the greeting message.
+*/
+
+/*
+Task 2: Add a method to the Person class that updates the age property and logs the updated age.
+*/
+
+/*
+Task 3: Define a class student that extends the Person class. Add a property studentId and a method to return the student ID. Create an instance of the Student class and log the student ID.
+*/
+
+/*
+Task 4: Override the greeting method in the Student class to include the Student ID in the message. Log the overridden greeting message.
+*/
+
+/*
+Task 5: Add a static method to the Person class that returns a generic greeting message. Call this static method without creating of the class and log the message.
+*/
+
+/*
+Task 6: Add a static property to the Student class to keep of the number of student created. Increment this property in the constructor and log the total number of students.
+*/
+
+/*
+Task 7: Add a getter method to the Person class to return the full name(assume a firstName and lastName property). Create an instance and log the full name using the getter.
+
+*/
+
+/*
+Task 8: Add a setter method to the Person class to update the name properties (firstName and lastName). Update the name using the setter and log the updated full name.
+*/
+
+/*
+
+Task 9: Define a class Account with private fields for balance and a method to deposit and withdraw money. Ensure that the balance can only be updated through these methods.
+*/
+
+/*
+Task 10: Create an instance of the Account class and test the deposit and withdraw methods, logging the balance after each operation.
+*/
